4b2bfa2655a646345a87e743a88657a7241ddcf5
rey-arx <raihan786arx@gmail.com>
2022-07-07 00:01:48 +0530

    Delete in crud

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,11 +1,34 @@
-import React,{useState} from 'react'
-import firebase from '../utils/firebase';
-import {db} from "../utils/firebase";
-import {collection,addDoc,Timestamp} from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import firebase from "../utils/firebase";
+import { db } from "../utils/firebase";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+  Timestamp,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const Forms = () => {
-    const [title,setTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
 
+  //Getting list from Firestore
+  useEffect(() => {
+    const q = query(
+      collection(db,"tasks"), orderBy('created', 'asc')
+    );
+
+   onSnapshot(q, (snapshot) => {
+      setTasks(snapshot.docs.map((doc) => ({ id: doc.id,
+        data: doc.data() })));
+      setTitle("");
+    });
+    
+  }, []);
 
   /* using realtime Database 
   const handleOnChange = (e) =>{
@@ -21,21 +44,56 @@ const Forms = () => {
     todoRef.push(todo);
   };*/
   const handleSubmit = async (e) => {
-    e.preventDefault()
-      await addDoc(collection(db, 'tasks'), {
-        title: title,
-        completed: false,
-        created: Timestamp.now()
-      })
+    e.preventDefault();
+
+    //adding items
+    await addDoc(collection(db, "tasks"), {
+      title: title,
+      completed: false,
+      created: Timestamp.now(),
+    });
+
   };
+
+  //Deleting Items
+  async function handleDelete(id) {
+    let request = await deleteDoc(doc(db, "tasks", id));
+    console.log(request)
+  }
+  /*old use effect
+  useEffect(() => {
+    const q = query(collection(db, 'tasks'), orderBy('created', 'desc'))
+    onSnapshot(q, (querySnapshot) => {
+      setTitle(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+  },[])
+*/
+const oi = () =>{
+  console.log("eee");
+}
   return (
     <div>
-        <h1>oii</h1>
-        <input type = "text" onChange={(e) => setTitle(e.target.value.toUpperCase())} 
-          value={title}></ input>
-        <button  onClick={handleSubmit}>Add todo</button>
+      <h1>oii</h1>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value.toUpperCase())}
+        value={title}
+      ></input>
+      <button onClick={handleSubmit}>Add todo</button>
+      <button onClick={oi}>hi</button>
+      <div>
+        {tasks.map((task) => (
+          <div>   
+          {task.data.title}         
+          <button className='task__deleteButton' onClick={() => handleDelete(task.id)}>Delete</button>
+          </div>
+        ))} 
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Forms;
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,13 +6,14 @@ import {getFirestore} from '@firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "fb.env.REACT_APP_apiKey",
-  authDomain: "fb.env.REACT_APP_authDomain",
-  projectId: "fb.env.REACT_APP_projectId",
-  storageBucket: "fb.env.REACT_APP_storageBucket",
-  messagingSenderId: "945967509495fb.env.REACT_APP_storageBucket",
-  appId: "fb.env.REACT_APP_appId",
-  measurementId: "fb.env.REACT_APP_measurementId"
+  apiKey: "AIzaSyBpwry05BEK2T5e_WhSePil2QJght1kh_w",
+  authDomain: "skcet-it-library-managem-3df37.firebaseapp.com",
+  databaseURL: "https://skcet-it-library-managem-3df37-default-rtdb.firebaseio.com",
+  projectId: "skcet-it-library-managem-3df37",
+  storageBucket: "skcet-it-library-managem-3df37.appspot.com",
+  messagingSenderId: "945967509495",
+  appId: "1:945967509495:web:7fcfe922600a9497be08f2",
+  measurementId: "G-RDS8N1LMSD"
      };
 
      const app = initializeApp(firebaseConfig);
