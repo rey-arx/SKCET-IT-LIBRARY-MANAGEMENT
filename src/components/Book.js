6c95cc55034e7ece38298947c97005d49382d77c
rey-arx <raihan786arx@gmail.com>
2022-07-09 20:36:28 +0530

    Table Created with Read operation

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React ,{useState, useEffect}from "react";
+import { db } from "../utils/firebase";
+import {
+  collection,
+  query,
+  addDoc,
+  updateDoc,
+  orderBy,
+  doc,
+  deleteDoc,
+  onSnapshot,
+  Timestamp,
+  setDoc,
+} from "firebase/firestore";
 import Addbook from "./Addbook";
 import './stylesmain.css' 
+import './table.css' 
 
 const Book = (props) => {
-    const Addbk = () =>{
-        props.setPage = (<Addbook />)
-    }
+
+  const [data,setData] = useState("");
+  const [book, getBook] = useState([]);
+
+
+  useEffect(() => {
+    const q = query(collection(db, "tasks"), orderBy("created", "desc"));
+    onSnapshot(q, (querySnapshot) => {
+      getBook(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+  
+  
   return (
     <div>
         <h1>Book Management</h1>
@@ -30,6 +59,24 @@ const Book = (props) => {
             </div>
           </div>
         </div>
+        
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Completed</th>
+              <th>created</th>
+            </tr>
+          </thead>
+          <tbody>
+            {book.map((books) => (
+              <tr>
+                <td>{books.data.title}</td>
+                
+              </tr>
+            ))}
+          </tbody>
+        </table>
       
     </div>
   );
